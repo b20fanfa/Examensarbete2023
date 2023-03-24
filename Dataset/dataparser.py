631f9ccbf25ad39c793834fc2b140bb0b4deb23b
b20fanfa <b20fanfa@student.his.se>
2023-03-24 12:35:55 +0100

    Heatmap.js with the correct dataset
    Created a python parser file, which finds the correct dataset. Lat and Long.
    
    The correct dataset that is implemented in the heatmaps.js artifact

--- Dataset/dataparser.py
+++ Dataset/dataparser.py
@@ -5,25 +5,28 @@ import json
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments , def is a function)
 def make_json(csvFilePath, jsonFilePath):
-     
+
   # create a dictionary
   data = []
   obj = {}
     
   # Open a csv reader called DictReader
   with open(csvFilePath, encoding='utf-8') as csvf:
-    csvReader = csv.DictReader(csvf ,delimiter="'")
+    csvReader = csv.DictReader(csvf ,delimiter="\t")
         
-      # Convert each row into a dictionary
-      # and add it to data
+    # Convert each row into a dictionary
+    # and add it to data
     for i, rows  in enumerate(csvReader):
-        # be the primary key
-        if i <10:
-          # specifies which data to use. 
-          # The "attribute" is the key, and the value is the value of said key {"attribute": value/Latitude}
-          data.append({"Latitude": rows["Latitude"], "Longitude": rows['Longitude']})
+      
+      # # how many datapoint that will be displayed
+      if i <100:
+          
+        # specifies which data to use. 
+        # The "attribute" is the key, and the value is the value of said key {"attribute": value/Latitude}
+        print(rows)
+        data.append({"Latitude": rows["Latitude [degrees_north]"], "Longitude": rows['Longitude [degrees_east]']})
 
-    obj["data"] = data
+  obj["data"] = data
 
   # Open a json writer, and use the json.dumps()
   # function to dump data
