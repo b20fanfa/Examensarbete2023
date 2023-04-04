e5b69b45930f6d4a5129668a65198fcef33eb4e5
b20fanfa <b20fanfa@student.his.se>
2023-04-04 16:27:40 +0200

    I fixed Tampermpnkey script
    Fixed the problem, that the script reloaded 10 times and it appeared in localstroage.

--- Tampermonkey/Tampermonkey.js
+++ Tampermonkey/Tampermonkey.js
@@ -15,16 +15,18 @@
   'use strict';
   //checks if the value "index" is null
     if(localStorage.getItem("index") === null){
+      localStorage.setItem('index', 1); // added this code to always add 1 
   }
 
   let a = parseInt(localStorage.getItem('index'));
-  localStorage.setItem('start' + a, performance.now());
+  localStorage.setItem('start' + a, performance.now()); // performance.now() show milliseconds
 
   //Prints start time, end time and the total time for each interval
   window.addEventListener('load', function() {
-    localStorage.setItem('end' + a, performance.now());
+    localStorage.setItem('end' + a, performance.now()); // performance.now() method returns a high resolution timestamp in milliseconds
     localStorage.setItem('time_counter' + a, (localStorage.getItem("end" + a) -localStorage.getItem("start" + a)));
-   
+    localStorage.setItem('index', a + 1); //  always add 1 in the value index in localStorage
+
     //how many times it should iterate
     if (a < 10 ) {
       location.reload();
